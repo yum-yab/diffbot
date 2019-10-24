bc5f22a3b8c9293b9cf2ca37ed0d0ebe61bc7df1
Denis Streitmatter <streitmatter@informatik.uni-leipzig.de>
2019-10-24 13:13:28 +0200

    Better use of the bash-script, corrected gitignore

--- src/main/scala/org/dbpedia/diffbot/DiffHandler.scala
+++ src/main/scala/org/dbpedia/diffbot/DiffHandler.scala
@@ -84,13 +84,13 @@ class DiffHandler(val config :Config, val datasets : List[Dataset] ,val diffVers
     runMavenCommand(new io.File(s"$localDir/$diffId/pom.xml"), List("-B","versions:set", s"-DnewVersion=$diffVersion"))
 
     // deploy the release (config for that in the pom file)
-    runMavenCommand(new io.File(s"$localDir/$diffId/pom.xml"), List("-B", "deploy"))
+    //runMavenCommand(new io.File(s"$localDir/$diffId/pom.xml"), List("-B", "deploy"))
     logger.info(s"Finished all datasets and deploying to the databus")
   }
 
 
   private def diffFiles(oldFilePath : String, newFilePath : String, target : String): Boolean = {
-    val returncode = Process(Seq("/usr/bin/bash",scriptpath, oldFilePath, newFilePath, target)) !!;
+    val returncode = Process(Seq("/bin/bash",scriptpath, oldFilePath, newFilePath, target)) !!;
 
     if (returncode == 0) true else false
   }
